e720254462a800c5f772aff56c72fe2f890280f8
Luciano Tonet <tonetlds@gmail.com>
2016-02-17 08:48:27 -0200

    Adicionado campo LOGO para locatários. fixes#1

--- database/migrations/2016_02_16_191611_update_locatario_table.php
+++ database/migrations/2016_02_16_191611_update_locatario_table.php
@@ -1,53 +1,48 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class UpdateLocatarioTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('handles', function($table)
-        {
-            $table->integer('romaneio_id')->unsigned();
-            $table->foreign('romaneio_id')->references('id')->on('romaneios')->onDelete('cascade');
-        });
+class UpdateLocatarioTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('handles', function ($table) {
+			$table->integer('romaneio_id')->unsigned();
+			$table->foreign('romaneio_id')->references('id')->on('romaneios')->onDelete('cascade');
+		});
 
-        Schema::table('locatarios', function($table)
-        {
-            $table->string('IPexterno')->nullable();
-            $table->string('usuarioFTP')->nullable();
-            $table->string('senhaFTP')->nullable();
-            $table->string('portaFTP')->nullable();
-            $table->string('IPinterno')->nullable();
-            
-        });
-    }
+		Schema::table('locatarios', function ($table) {
+			$table->string('IPexterno')->nullable();
+			$table->string('IPinterno')->nullable();
+			$table->string('FTPusuario')->nullable();
+			$table->string('FTPsenha')->nullable();
+			$table->string('FTPporta')->nullable();
+			$table->string('logo')->nullable();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-       Schema::table('handles', function (Blueprint $table) {
-            $table->dropColumn('romaneio_id');
-            $table->dropForeign('handles_romaneio_id_foreign');
-        });
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('handles', function (Blueprint $table) {
+			$table->dropColumn('romaneio_id');
+			$table->dropForeign('handles_romaneio_id_foreign');
+		});
 
-       Schema::table('locatarios', function (Blueprint $table) {
-            $table->dropColumn('IPexterno');
-            $table->dropColumn('usuarioFTP');
-            $table->dropColumn('senhaFTP');
-            $table->dropColumn('portaFTP');
-            $table->dropColumn('IPinterno');
-        });
-    }
+		Schema::table('locatarios', function (Blueprint $table) {
+			$table->dropColumn('IPexterno');
+			$table->dropColumn('IPinterno');
+			$table->dropColumn('FTPusuario');
+			$table->dropColumn('FTPsenha');
+			$table->dropColumn('FTPporta');
+			$table->dropColumn('logo');
+		});
+	}
 }
-
